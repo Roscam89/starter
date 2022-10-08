af750473225d0b947489173b7745fce42676466b
max <lil_max2002@yahoo.com>
2022-10-08 14:58:24 -0700

    firstComit 	new file:   .prettierrc 	new file:   img/card-lazy.jpg 	new file:   img/card.jpg 	new file:   img/digital-lazy.jpg 	new file:   img/digital.jpg 	new file:   img/grow-lazy.jpg      new file:   img/grow.jpg 	new file:   img/hero.png 	new file:   img/icon.png 	new file:   img/icons.svg 	new file:   img/img-1.jpg 	new file:   img/img-2.jpg 	new file:   img/img-3.jpg 	new file:   img/img-4.jpg 	new file:   img/logo.png 	new file:   img/user-1.jpg 	new file:   img/user-2.jpg 	new file:   img/user-3.jpg 	new file:   index.html 	new file:   script.js 	new file:   style.css

---
+++ script.js
@@ -0,0 +1,92 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn=>btn.addEventListener("click", openModal));
+
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+////////////////////////////////////////////
+///////////////////////////////////////////
+
+
+//----------------------Selecting elements ex----------------------//
+
+//console.log(document.documentElement);
+//console.log(document.head);
+//console.log(document.body);
+
+const header = document.querySelector(".header");
+
+const allSelections = document.querySelectorAll(".section");
+//console.log(allSelections);
+
+document.getElementById("section--1");
+const allButtons = document.getElementsByTagName("button");
+//console.log(allButtons);
+
+//console.log(document.getElementsByClassName("btn"));
+
+
+//-------------Creating and inserting elemets ex-------------------//
+
+const message = document.createElement("div");
+
+message.classList.add("cookie-message");
+
+message.innerHTML = `We use cookies for improved functionality and analytics. 
+<button class="btn 
+btn--close-cookie"> Got it</button>`;
+
+
+//header.append(message);
+
+//header.prepend(message);
+
+//header.before(message.cloneNode(true));
+
+header.after(message);
+
+
+//-----------Delete elements----------//
+
+ const btnCookie=document.querySelector(".btn--close-cookie");
+
+
+
+btnCookie.addEventListener("click",()=>{
+// message.innerHTML=""
+
+message.remove();
+});
+
+
+
+
+
+
